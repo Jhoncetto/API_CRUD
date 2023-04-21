63653215744363f20e303dc244d232b4a6231804
Jhoncetto <jhoseba1@gmail.com>
2023-04-20 22:50:52 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import app from './app'
-import './database'
-import {PORT} from './config'
-require('dotenv').config()
+import app from "./app";
+import "./database";
+import { PORT } from "./config";
+require("dotenv").config();
 
-const port = PORT
-app.listen(port, console.log("Conectado al puerto", port))
+const port = PORT;
+app.listen(port, console.log("Conectado al puerto", port));
